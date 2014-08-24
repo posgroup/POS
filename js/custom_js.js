65642c6c4b7f7fb1a09d3882237665b60e381c5a
Yashwant Dhakad <yashwant.dhakad@hotwaxmedia.com>
2014-08-24 20:17:59 +0530

    Added full screen mode functionality for all pages.

--- js/custom_js.js
+++ js/custom_js.js
@@ -109,6 +109,56 @@ jQuery(function() {
           jQuery(this).parent().css({'overflow-y':'auto'});
       }
   });
+  
+  addEventListener("click", function() {
+      var
+            el = document.documentElement
+          , rfs =
+                 el.requestFullScreen
+              || el.webkitRequestFullScreen
+              || el.mozRequestFullScreen
+      ;
+      rfs.call(el);
+  });
+  function isFullScreen()
+  {
+      return (document.fullScreenElement && document.fullScreenElement !== null)
+           || document.mozFullScreen
+           || document.webkitIsFullScreen;
+  }
+
+
+  function requestFullScreen(element)
+  {
+      if (element.requestFullscreen)
+          element.requestFullscreen();
+      else if (element.msRequestFullscreen)
+          element.msRequestFullscreen();
+      else if (element.mozRequestFullScreen)
+          element.mozRequestFullScreen();
+      else if (element.webkitRequestFullscreen)
+          element.webkitRequestFullscreen();
+  }
+
+  function exitFullScreen()
+  {
+      if (document.exitFullscreen)
+          document.exitFullscreen();
+      else if (document.msExitFullscreen)
+          document.msExitFullscreen();
+      else if (document.mozCancelFullScreen)
+          document.mozCancelFullScreen();
+      else if (document.webkitExitFullscreen)
+          document.webkitExitFullscreen();
+  }
+
+  function toggleFullScreen(element)
+  {
+      if (isFullScreen())
+          cancelFullScreen();
+      else
+          requestFullScreen(element || document.documentElement);
+  }
   default_modal_options = {
       replace: true
   }
